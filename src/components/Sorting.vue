2013b89d3c72859509b72fe6bc1fbc33fcd2e360
Sutan Syahdinullah S <ssyahdinullahs@gmail.com>
2021-03-25 19:16:43 +0700

    Adding Merge Sort Algorithm

--- src/components/Sorting.vue
+++ src/components/Sorting.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="container">
     <div class="side-bar">
-      <button v-on:click="generateArray(100, 5, 600)">
+      <button v-on:click="generateRandomArray(250, 5, 600)">
         Generate new Array
       </button>
       <button v-on:click="bubbleSort(arr)">Bubble Sort</button>
+      <button v-on:click="startMergeSort(arr)">Merge Sort</button>
     </div>
     <div class="content">
       <div
@@ -18,10 +19,14 @@
 </template>
 
 <script>
+const CHECKING_COLOR = "red";
+const PRIMARY_COLOR = "turquoise";
+const CLEARANCE_COLOR = "#12f320";
+
 export default {
   name: "Sorting",
   created: function () {
-    this.generateArray(100, 5, 600);
+    this.generateRandomArray(250, 5, 600);
   },
   data: function () {
     return {
@@ -30,7 +35,136 @@ export default {
     };
   },
   methods: {
-    generateArray: function (length, min, max) {
+    /**************** START BUBBLE SORT SECTION ***************/
+    /**
+     * Main function Bubble Sort Algorithm
+     * Tracking all progress and put into animation
+     * Run animation after sorting was done
+     * @param arr|Array
+     */
+    bubbleSort: function (arr) {
+      let isSorted = false;
+      let len = this.arr.length - 1;
+      const animations = [];
+      while (!isSorted) {
+        isSorted = true;
+        for (let i = 0; i < len; i++) {
+          animations.push([i, i + 1, false]);
+          if (arr[i] > arr[i + 1]) {
+            animations.push([i, arr[i + 1], undefined]);
+            animations.push([i + 1, arr[i], undefined]);
+            this.swap(arr, i, i + 1);
+            isSorted = false;
+          }
+          animations.push([i, i + 1, true]);
+        }
+        len--;
+      }
+
+      for(let i = 0; i < this.arr.length; i++) animations.push([i, this.arr[i], undefined])
+
+      this.animationSorting(animations);
+    },
+    /**************** END BUBLE SORT SECTION ***************/
+
+    /**************** START MERGE SORT SECTION ***************/
+    startMergeSort: function (arr) {
+      const animations = [];
+      if (arr.length < 1) return;
+      const jsArr = arr.slice().sort((a, b) => a - b);
+      const tempArr = arr.slice();
+      this.mergeSort(arr, tempArr, 0, arr.length - 1, animations);
+      this.animationSorting(animations);
+    },
+    /**
+     * Main function Insertion Sort Algorithm
+     * Sort half of given array
+     * store as tempArr
+     * @param arr|Array
+     * @param tempArr|Array
+     * @param startIndex|int
+     * @param endIndex|int
+     * @param animations|Array
+     */
+    mergeSort: function (
+      arr,
+      tempArr = [],
+      startIndex = null,
+      endIndex = null,
+      animations = []
+    ) {
+      if (startIndex >= endIndex) return;
+
+      const middleIndex = Math.floor((startIndex + endIndex) / 2);
+      this.mergeSort(tempArr, arr, startIndex, middleIndex, animations);
+      this.mergeSort(tempArr, arr, middleIndex + 1, endIndex, animations);
+      this.mergeHalves(arr, tempArr, startIndex, endIndex, animations);
+    },
+
+    /**
+     * Do Merge between 2 tempArray
+     * Tracking all progress and put into animation
+     * Run animation after sorting was done
+     * @param arr|Array
+     * @param tempArr|Array
+     * @param startIndex|int
+     * @param endIndex|int
+     * @param animations|Array
+     */
+    mergeHalves: function (arr, tempArr, startIndex, endIndex, animations) {
+      const leftEnd = Math.floor((startIndex + endIndex) / 2);
+      const rightStart = leftEnd + 1;
+
+      let anims = [];
+
+      let left = startIndex;
+      let right = rightStart;
+      let index = startIndex;
+
+      while (left <= leftEnd && right <= endIndex) {
+        animations.push([left, right, false]);
+        animations.push([left, right, true]);
+        if (tempArr[left] <= tempArr[right]) {
+          anims.push([index, tempArr[left], undefined]);
+          arr[index] = tempArr[left];
+          left++;
+        } else {
+          anims.push([index, tempArr[right], undefined]);
+          arr[index] = tempArr[right];
+          right++;
+        }
+        index++;
+      }
+
+      while (left <= leftEnd) {
+        animations.push([left, left, false]);
+        animations.push([left, left, true]);
+        anims.push([index, tempArr[left], undefined]);
+        arr[index] = tempArr[left];
+        left++;
+        index++;
+      }
+
+      while (right <= endIndex) {
+        animations.push([right, right, false]);
+        animations.push([right, right, true]);
+        anims.push([index, tempArr[right], undefined]);
+        arr[index] = tempArr[right];
+        right++;
+        index++;
+      }
+
+      for (let i = 0; i < anims.length; i++) animations.push(anims[i]);
+    },
+    /**************** END MERGE SORT SECTION ***************/
+
+    /**
+     * Generate Random Integer Array
+     * @param length|int
+     * @param min|int
+     * @param max|int
+     */
+    generateRandomArray: function (length, min, max) {
       this.arr = [];
       this.maxNum = -Infinity;
       for (let i = 0; i < length; i++) {
@@ -40,39 +174,34 @@ export default {
       }
     },
 
-    // From https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+    /**
+     * Generate Random Integer
+     * @param min|int
+     * @param max|int
+     *
+     * source: https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+     */
     randNumber: function (min, max) {
       return Math.floor(Math.random() * (max - min + 1) + min);
     },
 
+    /**
+     * Base function for swaping item in given array
+     * @param arr|Array
+     * @param indexA|int
+     * @param indexB|int
+     */
     swap: function (arr, indexA, indexB) {
       let temp = arr[indexA];
       arr[indexA] = arr[indexB];
       arr[indexB] = temp;
     },
 
-    bubbleSort: function (arr) {
-      let isSorted = false;
-      let len = this.arr.length - 1;
-      const animations = [];
-      while (!isSorted) {
-        isSorted = true;
-        for (let i = 0; i < len; i++) {
-          animations.push([i, i + 1, false])
-          if (arr[i] > arr[i + 1]) {
-            animations.push([i, arr[i + 1], undefined])
-            animations.push([i + 1, arr[i], undefined])
-            this.swap(arr, i, i + 1);
-            isSorted = false;
-          }
-          animations.push([i, i + 1, true])
-        }
-        len--;
-      }
-
-      this.animationSorting(animations);
-    },
-
+    /**
+     * Base function for animation, coloring and change height bar using given animations
+     * from sorting algorithm function
+     * @param animations|Array
+     */
     animationSorting: function (animations) {
       for (let i = 0; i < animations.length; i++) {
         const sortingBar = document.getElementsByClassName("arr-bar");
@@ -80,7 +209,7 @@ export default {
         if (c !== undefined) {
           const barAStyle = sortingBar[a].style;
           const barBStyle = sortingBar[b].style;
-          const color = !c ? "red" : "turquoise";
+          const color = !c ? CHECKING_COLOR : CLEARANCE_COLOR;
           setTimeout(() => {
             barAStyle.backgroundColor = color;
             barBStyle.backgroundColor = color;
@@ -89,6 +218,7 @@ export default {
           setTimeout(() => {
             const barAStyle = sortingBar[a].style;
             barAStyle.height = `${b}px`;
+            barAStyle.backgroundColor = PRIMARY_COLOR;
           }, i * 5);
         }
       }
@@ -96,14 +226,14 @@ export default {
 
     /**
      * checking function for result after sorting
-     * checking with build-in sorting function from javascript
-     * @param arrayOne|Array
-     * @param arrayTwo|Array
+     * check with build-in sorting function from javascript
+     * @param ownSortedArray|Array
+     * @param javascriptSortedArray|Array
      */
-    arrayAreEqual: function (arrayOne, arrayTwo) {
-      if (arrayOne.length !== arrayTwo.length) return false;
-      for (let i = 0; i < arrayOne.length; i++) {
-        if (arrayOne[i] !== arrayTwo[i]) {
+    arrayAreEqual: function (ownSortedArray, javascriptSortedArray) {
+      if (ownSortedArray.length !== javascriptSortedArray.length) return false;
+      for (let i = 0; i < ownSortedArray.length; i++) {
+        if (ownSortedArray[i] !== javascriptSortedArray[i]) {
           return false;
         }
       }
@@ -118,13 +248,13 @@ export default {
   display: grid;
 }
 .side-bar {
-  width: calc(100vw - 80%);
+  /* width: calc(100vw - 80%); */
+  width: 100vw;
   height: 100%;
-  border: 1px solid black;
 }
 .content {
   height: 95vh;
-  margin-top: 8px;
+  margin-top: 10px;
   border: 1px solid red;
   display: grid;
   justify-content: center;
@@ -132,9 +262,9 @@ export default {
   grid-auto-flow: column;
 }
 .arr-bar {
-  width: 10px;
+  width: 4px;
   height: 100px;
-  background: turquoise;
+  background: rgb(121, 121, 121);
   display: inline-block;
 }
 </style>
