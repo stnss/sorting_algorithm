46fb3dc1b85c6d72bdbc88f46836ce8ecb45aa3f
Sutan Syahdinullah S <ssyahdinullahs@gmail.com>
2021-03-25 15:25:29 +0700

    Finishing Bubble Sorting Algorithm

--- src/components/Sorting.vue
+++ src/components/Sorting.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="side-bar">
-      <button v-on:click="generateArray(425, 5, 600)">
+      <button v-on:click="generateArray(100, 5, 600)">
         Generate new Array
       </button>
       <button v-on:click="bubbleSort(arr)">Bubble Sort</button>
@@ -11,7 +11,7 @@
         class="arr-bar"
         v-for="(item, index) in arr"
         :key="index"
-        v-bind:style="{ height: item + 'px' }"
+        v-bind:style="{ height: (item / maxNum) * 99 + '%' }"
       ></div>
     </div>
   </div>
@@ -21,21 +21,23 @@
 export default {
   name: "Sorting",
   created: function () {
-    this.generateArray(425, 5, 600);
+    this.generateArray(100, 5, 600);
   },
   data: function () {
     return {
       arr: [],
+      maxNum: 0,
     };
   },
   methods: {
     generateArray: function (length, min, max) {
       this.arr = [];
+      this.maxNum = -Infinity;
       for (let i = 0; i < length; i++) {
-        this.arr.push(this.randNumber(min, max));
+        let temp = this.randNumber(min, max);
+        this.arr.push(temp);
+        this.maxNum = Math.max(this.maxNum, temp);
       }
-
-      console.log(this.arr);
     },
 
     // From https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
@@ -52,15 +54,43 @@ export default {
     bubbleSort: function (arr) {
       let isSorted = false;
       let len = this.arr.length - 1;
+      const animations = [];
       while (!isSorted) {
         isSorted = true;
         for (let i = 0; i < len; i++) {
+          animations.push([i, i + 1, false])
           if (arr[i] > arr[i + 1]) {
+            animations.push([i, arr[i + 1], undefined])
+            animations.push([i + 1, arr[i], undefined])
             this.swap(arr, i, i + 1);
             isSorted = false;
           }
+          animations.push([i, i + 1, true])
         }
         len--;
+      }
+
+      this.animationSorting(animations);
+    },
+
+    animationSorting: function (animations) {
+      for (let i = 0; i < animations.length; i++) {
+        const sortingBar = document.getElementsByClassName("arr-bar");
+        const [a, b, c] = animations[i];
+        if (c !== undefined) {
+          const barAStyle = sortingBar[a].style;
+          const barBStyle = sortingBar[b].style;
+          const color = !c ? "red" : "turquoise";
+          setTimeout(() => {
+            barAStyle.backgroundColor = color;
+            barBStyle.backgroundColor = color;
+          }, i * 5);
+        } else {
+          setTimeout(() => {
+            const barAStyle = sortingBar[a].style;
+            barAStyle.height = `${b}px`;
+          }, i * 5);
+        }
       }
     },
 
@@ -98,12 +128,11 @@ export default {
   border: 1px solid red;
   display: grid;
   justify-content: center;
-
   gap: 1px;
   grid-auto-flow: column;
 }
 .arr-bar {
-  width: 2px;
+  width: 10px;
   height: 100px;
   background: turquoise;
   display: inline-block;
